611a3b3632460d1c3ca41a50a2371feacb803052
Shubham Bayas <shubham.bayas@joshsoftware.com>
2025-02-10 11:14:25 +0530

    Stable point without jwt

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,16 +2,5 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   #  :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-<<<<<<< Updated upstream
          :recoverable, :rememberable, :validatable, :confirmable
-=======
-<<<<<<< Updated upstream
-         :recoverable, :rememberable, :validatable
-=======
-         :recoverable, :rememberable, :validatable, :confirmable
-
-  validates :name, presence: true
-  
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 end
