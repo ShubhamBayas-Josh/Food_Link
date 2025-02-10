dc524debb28d1af67044bbfaf024d333ce9559e4
Shubham Bayas <shubham.bayas@joshsoftware.com>
2025-02-10 11:45:26 +0530

    Updated

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,15 +1,7 @@
 # # require_relative "../json_web_token.rb"
 
-# class Api::V1::ApplicationController < ActionController::API
-#   include JsonWebToken
-
-#   before_action :authenticate_request
-
-#   private
-#   def authenticate_request
-#     header = request.headers["Authorization"]
-#     header = header.split(" ").last if header
-#     decoded = JsonWebToken.decode(header)
-#     @current_admin = Admin.find(decoded[:admin_id])
-#   end
-# end
+class Api::V1::ApplicationController < ActionController::API
+  def index
+  render json: { message: "Hello from Api::V1::ApplicationController" }
+  end
+end
