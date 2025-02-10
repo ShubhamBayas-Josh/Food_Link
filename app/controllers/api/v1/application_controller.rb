904796bb65617cd3e890fa908f47be1d0f651605
Shubham Bayas <shubham.bayas@joshsoftware.com>
2025-02-10 11:43:17 +0530

    Updated

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,15 +1,15 @@
-# require_relative "../json_web_token.rb"
+# # require_relative "../json_web_token.rb"
 
-class Api::V1::ApplicationController < ActionController::API
-  include JsonWebToken
+# class Api::V1::ApplicationController < ActionController::API
+#   include JsonWebToken
 
-  before_action :authenticate_request
+#   before_action :authenticate_request
 
-  private
-  def authenticate_request
-    header = request.headers["Authorization"]
-    header = header.split(" ").last if header
-    decoded = JsonWebToken.decode(header)
-    @current_admin = Admin.find(decoded[:admin_id])
-  end
-end
+#   private
+#   def authenticate_request
+#     header = request.headers["Authorization"]
+#     header = header.split(" ").last if header
+#     decoded = JsonWebToken.decode(header)
+#     @current_admin = Admin.find(decoded[:admin_id])
+#   end
+# end
--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,21 +1,21 @@
-class Api::V1::AuthenticationController < Api::V1::ApplicationController
-  skip_before_action :authenticate_request
-  before_action :login_params, only: :login
-  def login
-    admin = Admin.find_by(email: login_params[:email])
-    if admin&.valid_password?(login_params[:password])
-      token = JsonWebToken.encode(admin_id: admin.id)
-      # time = Time.now + 24.hours.to_i
-      render json: { token: token }, status: :ok
-    else
-      render json: { error: "unauthorized user" }, status: :unauthorized
-    end
-  end
+# class Api::V1::AuthenticationController < Api::V1::ApplicationController
+#   skip_before_action :authenticate_request
+#   before_action :login_params, only: :login
+#   def login
+#     admin = Admin.find_by(email: login_params[:email])
+#     if admin&.valid_password?(login_params[:password])
+#       token = JsonWebToken.encode(admin_id: admin.id)
+#       # time = Time.now + 24.hours.to_i
+#       render json: { token: token }, status: :ok
+#     else
+#       render json: { error: "unauthorized user" }, status: :unauthorized
+#     end
+#   end
 
 
-  private
+#   private
 
-  def login_params
-    params.permit(:email, :password)
-  end
-end
+#   def login_params
+#     params.permit(:email, :password)
+#   end
+# end
